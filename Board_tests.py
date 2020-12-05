0cb409d91e3c89032f91984ebebe57170a5e0873
BrenCamp13 <60230289+BrenCamp13@users.noreply.github.com>
2020-12-05 14:53:58 -0600

    Add files via upload

--- Board_tests.py
+++ Board_tests.py
@@ -5,14 +5,11 @@ class BoardTests(unittest.TestCase):
 
     # Should place piece
     def testPlacingMan():
-        str firstTest = placingMan(self, 10) # Test invalid input (10) to validate except block
-        str excepText = "Couldn't get the input value")
+        excepText = "Couldn't get the input value"
 
-        str secondTest = placingMan(self, 1)
-        str invalText
+        secondTest = placingMan(self, 1)
 
         self.assertEqual(firstTest, excepText, "Should output " + excepText)
-        self.
 
     def testMoveMan():
         moveMan(self, 1, 3)
@@ -22,44 +19,47 @@ class BoardTests(unittest.TestCase):
         flyMan(self, 0, 1)
         flyMan(self, 1, 1) # Should bypass try block, print "You cannot fly this man")
 
-   def testIsWin():
-        bool firstTest = isWin(2)
-        bool secondTest = isWin(1)
-        bool thirdTest = isWin(4)
+   #def testIsWin():
+        #firstTest = isWin(2)
+        #secondTest = isWin(1)
+        #thirdTest = isWin(4)
         
-        self.assertTrue(firstTest, "Should return true")
-        self.assertFalse(secondTest, "Should return true")
-        self.assertFalse(thirdTest, "Should return false")
+        #self.assertTrue(firstTest, "Should return true")
+        #self.assertFalse(secondTest, "Should return true")
+        #self.assertFalse(thirdTest, "Should return false")
         
     def testIsMill():
-        ''' Player == 0, so this statement should return false '''
-        bool firstTest = isMill(self, 1, 0)
+        # Player == 0, so this statement should return false
+        firstTest = isMill(self, 1, 0)
         self.assertFalse(firstTest, "Should return false")
         
     def testRemoveMan():
-        str firstTest = removeMan(self, 10, 1) # Test invalid input (10) to validate except block
-        str excepText = "Input was either out of bounds or wasn't an integer"
+        firstTest = removeMan(1, 10)
+        excepText = "Input was either out of bounds or wasn't an integer"
 
-        str secondTest = removeMan(self, 1, -1)
-        str invalText = "Invalid position at: "
+        secondTest = removeMan(1, -1)
+        invalText = "Invalid position at: "
         
         self.assertEqual(firstTest, excepText, "Should output " + excepText)
         self.assertEqual(secondTest, invalText, "Should output " + invalText)
 
     def testIndexTransfer():
-        int firstTest = IndexTransfer(self, 35, 30)
-        int secondTest = IndexTransfer(self, 231, 226)
-        int thirdTest = IndexTransfer(self, 427, 128)
+        firstTest = IndexTransfer(self, 35, 30)
+        secondTest = IndexTransfer(self, 231, 226)
+        thirdTest = IndexTransfer(self, 427, 128)
         
         self.assertEqual(firstTest, 0, "Should return 0")
         self.assertEqual(secondTest, 16, "Should return 16")
         self.assertEqual(thirdTest, 10, "Should dreturn 10")
 
     def testAdjacentPos():
-        int firstTest = adjacentPos(self, 1)
-        int secondTest = adjacentPos(self, 4)
-        int thirdTest = adjacentPos(self, 7)
+        firstTest = adjacentPos(self, 1)
+        secondTest = adjacentPos(self, 4)
+        thirdTest = adjacentPos(self, 7)
 
         self.assertEqual(firstTest, [0, 2, 9], "Should return list containing 0, 2, and 9")
         self.assertEqual(secondTest, [2, 7, 12], "Should return list containing 2, 7, and 12")
         self.assertEqual(thirdTest, [4, 6], "Should return list containing 4 and 6")
+
+unittest.main()
+
